519fe926f03876d8030e5ab48ca98bc3021af669
Herry <herry13@gmail.com>
2013-07-21 03:01:57 +0100

    remove net_helper.rb

--- lib/sfpagent.rb
+++ lib/sfpagent.rb
@@ -6,7 +6,6 @@ require 'sfp'
 # internal dependencies
 libdir = File.expand_path(File.dirname(__FILE__))
 
-require libdir + '/sfpagent/net_helper.rb'
 require libdir + '/sfpagent/executor.rb'
 require libdir + '/sfpagent/runtime.rb'
 require libdir + '/sfpagent/module.rb'
--- sfpagent.gemspec
+++ sfpagent.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
 	s.name			= 'sfpagent'
-	s.version		= '0.1.0'
+	s.version		= '0.1.1'
 	s.date			= '2013-07-03'
 	s.summary		= 'SFP Agent'
 	s.description	= 'A Ruby implementation of SFP agent.'
