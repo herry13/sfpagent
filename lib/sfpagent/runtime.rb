554d112f269f806410af2075339e4e3e3653934d
Herry <herry13@gmail.com>
2013-07-21 08:12:45 +0100

    enable _parent link to Sfp::Runtime@modules

--- lib/sfpagent/runtime.rb
+++ lib/sfpagent/runtime.rb
@@ -110,6 +110,8 @@ class Sfp::Runtime
 		root.accept(Sfp::Visitor::ParentEliminator.new)
 		@modules, state = get_object_state(root, root, as_sfp)
 
+		@modules.accept(ParentGenerator)
+
 		state
 	end
 
@@ -131,6 +133,12 @@ class Sfp::Runtime
 		end
 	end
 
+	ParentGenerator = Object.new
+	def ParentGenerator.visit(name, value, parent)
+		value['_parent'] = parent if value.is_a?(Hash)
+		true
+	end
+
 	def execute_sequential_plan(plan)
 		puts 'Execute a sequential plan...'
 
@@ -146,120 +154,3 @@ class Sfp::Runtime
 		true
 	end
 end
-
-=begin
-		def execute(plan)
-			plan = JSON.parse(plan)
-			if plan['type'] == 'sequential'
-				execute_sequential(plan)
-			else
-				execute_parallel(plan)
-			end
-		end
-
-
-		def execute_sequential(plan)
-			puts 'Execute a sequential plan...'
-
-			plan['workflow'].each_index { |index|
-				action = plan['workflow'][index]
-				print "#{index+1}) #{action['name']} "
-
-				module_path, method_name = action['name'].pop_ref
-				mod = @modules.at?(module_path)[:_self]
-				raise Exception, "Cannot execute #{action['name']}!" if not mod.respond_to?(method_name)
-				if not mod.send method_name.to_sym, normalise_parameters(action['parameters'])
-					puts '[Failed]'
-					return false
-				end
-
-				puts '[OK]'
-			}
-			true
-		end
-
-		def execute_parallel(plan)
-			# TODO
-			puts 'Execute a parallel plan...'
-			false
-		end
-=end
-
-=begin
-		def plan
-			# generate initial state
-			task = { 'initial' => Sfp::Helper.to_state('initial', self.get_state(true)) }
-
-			# add schemas
-			@root.each { |k,v|
-				next if !v.is_a?(Hash) or v['_context'] != 'class'
-				task[k] = v
-			}
-
-			# add goal constraint
-			model = @root.select { |k,v| v.is_a?(Hash) and v['_context'] == 'object' }
-			goalgen = Sfp::Helper::GoalGenerator.new
-			model.accept(goalgen)
-			task['goal'] = goalgen.results
-
-			# remove old parent links
-			task.accept(Sfp::Visitor::ParentEliminator.new)
-
-			# reconstruct Sfp parent links
-			task.accept(Sfp::Visitor::SfpGenerator.new(task))
-
-			# solve and return the plan solution
-			planner = Sfp::Planner.new
-			planner.solve({:sfp => task, :pretty_json => true})
-		end
-=end
-
-
-
-=begin
-	module Helper
-		def self.create_object(name)
-			{ '_self' => name, '_context' => 'object', '_isa' => '$.Object', '_classes' => ['$.Object'] }
-		end
-
-		def self.create_state(name)
-			{ '_self' => name, '_context' => 'state' }
-		end
-
-		def self.to_state(name, value)
-			raise Exception, 'Given value should be a Hash!' if not value.is_a?(Hash)
-			value['_self'] = name
-			value['_context'] = 'state'
-			value
-		end
-
-		module Constraint
-			def self.equals(value)
-				{ '_context' => 'constraint', '_type' => 'equals', '_value' => value }
-			end
-
-			def self.and(name)
-				{ '_context' => 'constraint', '_type' => 'and', '_self' => name }
-			end
-		end
-
-		class GoalGenerator
-			attr_reader :results
-
-			def initialize
-				@results = Sfp::Helper::Constraint.and('goal')
-			end
-				
-			def visit(name, value, parent)
-				return false if name[0,1] == '_'
-				if value.is_a?(Hash)
-					return true if value['_context'] == 'object'
-					return false
-				end
-
-				@results[ parent.ref.push(name) ] = Sfp::Helper::Constraint.equals(value)
-				false
-			end
-		end
-	end
-=end
