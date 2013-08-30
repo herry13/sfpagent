7dec0543be88257f2915b95a3a1ad1e6c5d0864d
herry13 <herry13@gmail.com>
2013-08-30 03:50:29 +0100

    update BSig

--- lib/sfpagent/bsig.rb
+++ lib/sfpagent/bsig.rb
@@ -121,8 +121,8 @@ class Sfp::BSig
 		return :failure if operator.nil?
 
 Sfp::Agent.logger.info "[#{@mode}] Flaws: #{JSON.generate(flaws)}"
-		return :ongoing if not lock_operator(operator)
 Sfp::Agent.logger.info "[#{@mode}] Selected operator: #{operator['name']}"
+		return :ongoing if not lock_operator(operator)
 
 		next_pi = pi + 1
 		pre_local, pre_remote = split_preconditions(operator)
